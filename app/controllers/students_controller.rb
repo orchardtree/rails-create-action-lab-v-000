20d4dcd191ac528e357a7bf9d84d78ad3c1ce39c
Jesse Wenger <wengerj@gmail.com>
2021-04-26 17:58:38 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,5 +15,4 @@ class StudentsController < ApplicationController
   new_student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
   redirect_to new_student_path
   end
-
 end
