a47630cc77c2c070798b4d21b0b73f3cbefad972
Jesse Wenger <wengerj@gmail.com>
2021-04-26 19:15:54 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,9 @@ class StudentsController < ApplicationController
 
   def create
     binding.pry
+    @student = Student.new
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
     #new_student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     #redirect_to student_path
   end
